79bb960e9dba5322cfd1b11b12b5931980180a51
Jingyu Cao <jingyu_cao@hotmail.com>
2022-05-31 22:43:17 +0200

    adjust the website css

--- src/components/UI/Introduction.tsx
+++ src/components/UI/Introduction.tsx
@@ -10,12 +10,13 @@ const Introduction = (props: Props) => {
     const introStyle = {
         width: props.style.width,
         textAlign: 'center',
-        fontSize: '0.8rem',
+        fontSize: '1rem',
         padding: '0.1rem 1rem',
         color: '#666',
         top: '1rem',
-        position: props.style.position
-    }
+        position: props.style.position,
+        fontFamily: '"Work Sans", sans-serif'
+}
 
     return (
         <Card style={introStyle}>
